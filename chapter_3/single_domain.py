75b2d95a9e17b9fbc197d3bdd31d5687fe203d34
Nahom Garefo <harobina28@gmail.com>
2024-10-13 18:35:17 +0300

    single domain crawling methods with random number seeding

---
+++ chapter_3/single_domain.py
@@ -0,0 +1,28 @@
+from  urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import random,datetime
+
+
+
+# print(datetime.datetime.now())
+# print(type(datetime.datetime.now()))
+# new=datetime.datetime.now().timestamp()
+# print(new,type(new))
+
+random.seed(datetime.datetime.now().timestamp())
+
+def getLinks(articleUrl):
+    html=urlopen("http://en.wikipedia.org{}".format(articleUrl))
+    bs=BeautifulSoup(html,"html.parser")
+    
+    return bs.find("div",{'id':'bodyContent'}).find_all('a',href=re.compile('^(/wiki/)((?!:).)*$'))
+
+
+links=getLinks('/wiki/Kevin_Bacon')
+
+while len(links)>0:
+    newArticle=links[random.randint(0,len(links)-1)].attrs['href']
+    print(newArticle)
+    links=getLinks(newArticle)
+
